704e795462ea1fce3ca7da3f7de511e01c24ab1e
ismamamama1 <ismael.agb@hotmail.fr>
2022-06-30 19:54:59 +0200

    Updated inputs

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo-title-white.png";
 
 function Header({ isDashboard }) {
   const object = [
-    { value: "", display: "find by theme" },
+    { value: "", display: "Find by theme" },
     { value: "PQ", display: "Problème de papier toilette" },
   ];
   return (
@@ -12,14 +12,14 @@ function Header({ isDashboard }) {
 
         {isDashboard ? (
           <input
-            className="h-8 justify-center object-center place-items-center place-content-center "
-            placeholder="find by name"
+            className="mr-16 mb-6 bg-gray-50 shadow border border-gray-300 text-gray-900 rounded block w-30 p-2"
+            placeholder="Find by name"
           />
         ) : (
           ""
         )}
         {isDashboard ? (
-          <select className="h-8 mr-16">
+          <select className="mr-16 mb-6 bg-gray-50 shadow border border-gray-300 text-gray-900 rounded block w-30 p-2.5">
             {object.map((item) => {
               return <option value={item.value}> {item.display} </option>;
             })}
