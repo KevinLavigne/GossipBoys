0fffc3520337a76895aabe611e1acc70e112c1f5
Alexandre Genin <alexandre.genin.pro@gmail.com>
2022-06-30 18:40:41 +0200

    more fixes, for bitches

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -5,7 +5,7 @@
 import "../App.css";
 // import { HashLink as NavLink } from "react-router-hash-link";
 import * as yup from "yup";
-import { useNavigate } from "react-router-dom";
+
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
@@ -21,7 +21,6 @@ const schema = yup.object({
 });
 
 function LogIn() {
-  const navigate = useNavigate();
   const {
     handleSubmit,
     register,
@@ -39,27 +38,30 @@ function LogIn() {
   return (
     <div className="right">
       <div className="login-wrapper rounded-lg shadow-2xl">
-        <h1 className="hi pt-5 pb-5 ps-5 pe-5 ms-5 me-5">Hi, Bitchers</h1>
-        <input
-          type="text"
-          className="w-50 h-10 p-2 m-3 bg-white border-2 justify-evenly"
-          placeholder="Your Email"
-        />
-        <input
-          type="password"
-          className="w-50 h-10 p-2 m-3 bg-white border-2  justify-evenly"
-          placeholder="Your Password"
-        />
-        <button className="loginbtn rounded-full mb-5 mt-10">
-          <NavLink to="/Page1">Bitch In</NavLink>
-        </button>
-        <p className="mb-10 mt-10 pl-3 pr-3 text-white">
-          You dont have an account ? Get close from your company{" "}
-          <NavLink className="signup" to="/SignUp">
-            Sign Up
-          </NavLink>
-        </p>
-
+        <form className="login-wrapper" onSubmit={handleSubmit(onSubmit)}>
+          <h1 className="hi pt-5 pb-5 ps-5 pe-5 ms-5 me-5">Hi, Bitchers</h1>
+          <input
+            {...register("email", { required: true })}
+            type="text"
+            className="w-50 h-10 p-2 m-3 bg-white border-2
+          justify-evenly"
+            placeholder="Your Email"
+          />
+          <p>{errors.email?.message}</p>
+          <input
+            {...register("password", { required: true })}
+            type="password"
+            className="w-50 h-10 p-2 m-3 bg-white border-2  justify-evenly"
+            placeholder="Your Password"
+          />
+          <p>{errors.password?.message}</p>
+          <button type="submit" className="loginbtn rounded-full mb-5 mt-10">
+            Bitch In
+          </button>
+          <p className="mb-10 mt-10 pl-3 pr-3 text-white">
+            You dont have an account ? Get close from your company{" "}
+          </p>
+        </form>
       </div>
     </div>
   );
