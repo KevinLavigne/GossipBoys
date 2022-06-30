4245c1d18a820e000cee6024c73abd079db8004e
ClementPremartin <clementpremartin@gmail.com>
2022-06-30 16:17:38 +0200

    setup new tailwind class

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,13 +5,13 @@ import dashboardData from "../data/dashboardData";
 function Dashboard() {
   return (
     <div>
-      <h2 className="underline decoration-8 decoration-orange-300 mb-8 mt-8 text-[#79ADDC] text-6xl ml-36">
+      <h2 className="underline decoration-8 decoration-darkGrey mb-8 mt-8 text-darkRed text-6xl ml-36">
         Dashboard
       </h2>
       <div className="flex flex-col w-full h-3/4 justify-center items-center">
         {dashboardData.map((data) => (
           <li
-            className="flex flex-row w-11/12 h-full justify-center mr-8 topics"
+            className="flex flex-row w-11/12 h-full justify-center mr-8 transition duration-700 ease-out hover:scale-105"
             key={data.id}
           >
             <DashboardCards data={data} />
