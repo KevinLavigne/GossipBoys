89827dcf7e473be48cc9ee002e69827fd016209b
Guillaume-Guerit <100518697+Guillaume-Guerit@users.noreply.github.com>
2022-07-01 12:33:36 +0200

    Update TopicsManager.js

--- backend/src/models/TopicsManager.js
+++ backend/src/models/TopicsManager.js
@@ -16,6 +16,15 @@ class TopicsManager extends AbstractManager {
       [topics.title, topics.id]
     );
   }
+  
+  getTopicsByUserId(usersId) {
+    return this.connection.query(
+      `select t.id, t.title, t.date, t.reaction, t.category, t.users_id, u.email, u.id, u.pseudo from ${TopicsManager.table} as t
+        inner join users as u on t.users_id = u.id 
+        where t.id = ?`,
+      [usersId]
+    );
+  }
 }
 
 module.exports = TopicsManager;
